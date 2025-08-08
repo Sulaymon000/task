62a6f466ebd28bc4dbb8b1850138dc89d7b23ad7
Sulaymon000 <sulaymonsodikov600@gmail.com>
2025-08-08 20:15:29 +0500

    1_vazifa

--- app/main.js
+++ app/main.js
@@ -11,3 +11,27 @@
 
 //
 //2
+// let listItems = document.querySelectorAll("#numbers li");
+
+// listItems.forEach((item) => {
+//   const number = parseInt(item.textContent, 10);
+
+//   if (number % 2 === 0) {
+//     item.classList.add("even");
+//   } else {
+//     item.classList.add("odd");
+//   }
+// });
+//
+//
+//...3
+
+const names = ["Ali", "Vali", "Olim", "Zaynab", "Laylo"];
+
+const container = document.querySelector("#namesContainer");
+
+names.forEach((name) => {
+  const p = document.createElement("p");
+  p.textContent = name;
+  container.prepend(p);
+});
