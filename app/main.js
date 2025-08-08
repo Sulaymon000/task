69c8e9b021258c7cb8b3240be05168239896202d
Sulaymon000 <sulaymonsodikov600@gmail.com>
2025-08-08 18:30:29 +0500

    vazifa

---
+++ app/main.js
@@ -0,0 +1,13 @@
+// let container = document.getElementById("listContainer");
+
+// let ul = document.createElement("ul");
+
+// for (let i = 1; i <= 10; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = i;
+//   ul.append(li);
+// }
+// container.append(ul);
+
+//
+//2
